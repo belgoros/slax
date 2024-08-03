47296981911a26ed564217200e5f7ac4249888a1
belgoros <s.cambour@gmail.com>
2024-08-03 21:37:31 +0200

    Show room info in ChatRoomLive

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -1,9 +1,27 @@
 defmodule SlaxWeb.ChatRoomLive do
   use SlaxWeb, :live_view
 
+  alias Slax.Repo
+  alias Slax.Chat.Room
+
+  def mount(_params, _session, socket) do
+    room = Room |> Repo.all() |> List.first()
+
+    {:ok, assign(socket, :room, room)}
+  end
+
   def render(assigns) do
     ~H"""
-    <div>Welcome to the Chat!</div>
+    <div class="flex flex-col flex-grow shadow-lg">
+      <div class="flex items-center justify-between flex-shrink-0 h-16 px-4 bg-white border-b border-slate-300">
+        <div class="flex flex-col gap-1.5">
+          <h1 class="text-sm font-bold leading-none">
+            #<%= @room.name %>
+          </h1>
+          <div class="text-xs leading-none h-3.5"><%= @room.topic %></div>
+        </div>
+      </div>
+    </div>
     """
   end
 end
