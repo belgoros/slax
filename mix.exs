d9618192a3a8ba0581915db72e318958e2f1cf84
belgoros <s.cambour@gmail.com>
2024-08-04 15:05:13 +0200

    Remove makeup_eex

--- mix.exs
+++ mix.exs
@@ -59,8 +59,7 @@ defmodule Slax.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:makeup_eex, "~> 0.1.2"}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
